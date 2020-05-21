e9b944bf03a494a7c27780cd3ff9a9e55520fd70
Brad Carter <bradc1993@gmail.com>
2020-05-21 01:32:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name][key]
           final_array[name][key] = []
         end
-        final_array[name][key] << 
+        final_array[name][key] << value.to_s
         # binding.pry
       end
     end
